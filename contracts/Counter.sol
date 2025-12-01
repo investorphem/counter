90b300bd2fa209fbe24ddd530f78176bb16d7c86
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-01 21:11:14 +0100

    Add Counter contract with increment and retrieve functions

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -1,1 +1,15 @@
+/**
+ *Submitted for verification at basescan.org on 2025-07-30
+*/
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract Counter {
+    uint256 public number;
+    function increment() public {
+        number++;
+    }
+    function retrieve() public view returns (uint256) {
+        return number;
+    }
+}
